28852b7a1e97f3ad9f16eced83c8005cbefd2745
LuNicholas <nicholas.lu@student.howest.be>
2021-08-23 15:34:43 +0200

    Able to check if next node in given direction is valid

--- framework/EliteAI/EliteGraphs/EliteGraphAlgorithms/EJPS.h
+++ framework/EliteAI/EliteGraphs/EliteGraphAlgorithms/EJPS.h
@@ -24,16 +24,16 @@ namespace Elite
 					&& estimatedTotalCost == other.estimatedTotalCost;
 			};
 
-			bool operator<(const NodeRecord& other) const
-			{
-				return estimatedTotalCost < other.estimatedTotalCost;
-			};
 		};
 
 		std::vector<T_NodeType*> FindPath(T_NodeType* pStartNode, T_NodeType* pDestinationNode);
 
 	private:
 		float GetHeuristicCost(T_NodeType* pStartNode, T_NodeType* pEndNode) const;
+
+		T_NodeType* Jump(T_NodeType* Parent, int horizontal, int vertical, T_NodeType* pStartNode, T_NodeType* pEndNode) const;
+		//T_NodeType HorSearch(T_NodeType* Parent, int horizontal, int vertical, T_NodeType* pStartNode, T_NodeType* pEndNode) const;
+		//T_NodeType VerSearch(T_NodeType* Parent, int horizontal, int vertical, T_NodeType* pStartNode, T_NodeType* pEndNode) const;
 
 		IGraph<T_NodeType, T_ConnectionType>* m_pGraph;
 		Heuristic m_HeuristicFunction;
@@ -49,8 +49,42 @@ namespace Elite
 	template <class T_NodeType, class T_ConnectionType>
 	std::vector<T_NodeType*> JPS<T_NodeType, T_ConnectionType>::FindPath(T_NodeType* pStartNode, T_NodeType* pGoalNode)
 	{
+		Jump(pStartNode, -1, 0, pStartNode, pGoalNode);
+
 		return vector<T_NodeType*>();
 	}
+
+	template <class T_NodeType, class T_ConnectionType>
+	T_NodeType* Elite::JPS<T_NodeType, T_ConnectionType>::Jump(T_NodeType* parent, int horizontal, int vertical, T_NodeType* pStartNode, T_NodeType* pEndNode) const
+	{
+		//position of new node
+		int connections{ 0 };
+
+		T_NodeType* pNewNode = nullptr;
+		for (T_ConnectionType* connection : m_pGraph->GetNodeConnections(parent->GetIndex()))
+		{
+			connections++;
+			if (m_pGraph->GetNodePos(connection->GetTo()) == Vector2{ m_pGraph->GetNodePos(parent).x + horizontal,  m_pGraph->GetNodePos(parent).y + vertical })
+			{
+				pNewNode = m_pGraph->GetNode(connection->GetTo());
+				break;
+			}
+		}
+		//all connections were wrong
+		if (connections == m_pGraph->GetNodeConnections(parent->GetIndex()).size())
+			return NULL;//there is no connection to the next node wanted
+
+
+		//the new node found is the endNode
+		if (pNewNode == pEndNode)
+			return pNewNode;
+
+		
+
+
+	}
+
+
 
 	template <class T_NodeType, class T_ConnectionType>
 	float Elite::JPS<T_NodeType, T_ConnectionType>::GetHeuristicCost(T_NodeType* pStartNode, T_NodeType* pEndNode) const
@@ -58,4 +92,7 @@ namespace Elite
 		Vector2 toDestination = m_pGraph->GetNodePos(pEndNode) - m_pGraph->GetNodePos(pStartNode);
 		return m_HeuristicFunction(abs(toDestination.x), abs(toDestination.y));
 	}
+
+
+
 }
